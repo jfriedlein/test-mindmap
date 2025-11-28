562b6601f4c9b5c8c82832ce3b58b0a17afe0352
jfriedlein <58786257+jfriedlein@users.noreply.github.com>
2025-11-28 15:25:25 +0100

    test mm

---
+++ literature.mm
@@ -0,0 +1,150 @@
+<map version="freeplane 1.12.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks>
+    <bookmark nodeId="ID_696401721" name="Root" opensAsRoot="true"/>
+</bookmarks>
+<node TEXT="literature" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1764339662110" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_tags="UNDER_NODES" show_note_icons="true" show_icon_for_attributes="true" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="&amp;AutoDiff_&amp;AD_SymDiff_SD_&amp;AceGen_AG" ID="ID_916460408" CREATED="1718372603476" MODIFIED="1731073601209">
+<edge COLOR="#0000ff"/>
+<node TEXT="AceGen - manual.pdf" ID="ID_1417886142" CREATED="1718372612966" MODIFIED="1764339884065" LINK="./MA/AutoDiff_AD_SymDiff_SD_AceGen_AG/AceGen%20-%20manual.pdf">
+<attribute NAME="update_procedure" VALUE="update_new"/>
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      lhs⊨SMSIf[condition, t, f] ;x; okular-{ab8a057e-9968-4a4c-9156-103e1f4095b9}
+    </p>
+    <p>
+      lhs⊨SMSWhich[test1 ;x; okular-{290ead73-95fa-4987-ad80-c82fbdb2e8c4}
+    </p>
+    <p>
+      SMSSwitch[expr,form1 ;x; okular-{b1ee9285-e942-4303-9c64-36a652efef03}
+    </p>
+    <p>
+      nested if ;x; okular-{80c3a899-f8d4-41de-8b92-22392e31199a}
+    </p>
+    <p>
+      Rg ⊨ Jgd wgp (k (k Dδϕ.Dϕ - δϕ λ Q) ;x; 3a5abfdc-3380-432e-ae0f-2d87e116e1dc
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="lhs ⊨ SMSIf[condition, t, f]" ID="ID_1453591578" CREATED="1718372620770" MODIFIED="1718372710299" LINK="menuitem:_H2aOpenPdfOnAnnotPage_on_single_node">
+<attribute NAME="annot_page" VALUE="19" OBJECT="org.freeplane.features.format.FormattedNumber|19"/>
+<attribute NAME="annot_type" VALUE="highlight"/>
+<attribute NAME="annot_ID" VALUE="okular-{ab8a057e-9968-4a4c-9156-103e1f4095b9}"/>
+<attribute NAME="annot_modTime_PDF" VALUE="D:20240614154510+01&apos;00"/>
+<attribute NAME="annot_status" VALUE="ok"/>
+<attribute NAME="annot_modTime_Freeplane" VALUE="Fri Jun 14 15:45:10 CEST 2024"/>
+</node>
+<node TEXT="lhs ⊨ SMSWhich" ID="ID_654970656" CREATED="1718372620775" MODIFIED="1718372710300" LINK="menuitem:_H2aOpenPdfOnAnnotPage_on_single_node">
+<attribute NAME="annot_page" VALUE="19" OBJECT="org.freeplane.features.format.FormattedNumber|19"/>
+<attribute NAME="annot_type" VALUE="highlight"/>
+<attribute NAME="annot_ID" VALUE="okular-{290ead73-95fa-4987-ad80-c82fbdb2e8c4}"/>
+<attribute NAME="annot_modTime_PDF" VALUE="D:20240614154510+01&apos;00"/>
+<attribute NAME="annot_status" VALUE="ok"/>
+<attribute NAME="annot_modTime_Freeplane" VALUE="Fri Jun 14 15:45:10 CEST 2024"/>
+</node>
+<node TEXT="SMSSwitch" ID="ID_1886670350" CREATED="1718372620778" MODIFIED="1718372710301" LINK="menuitem:_H2aOpenPdfOnAnnotPage_on_single_node">
+<attribute NAME="annot_page" VALUE="19" OBJECT="org.freeplane.features.format.FormattedNumber|19"/>
+<attribute NAME="annot_type" VALUE="highlight"/>
+<attribute NAME="annot_ID" VALUE="okular-{b1ee9285-e942-4303-9c64-36a652efef03}"/>
+<attribute NAME="annot_modTime_PDF" VALUE="D:20240614154510+01&apos;00"/>
+<attribute NAME="annot_status" VALUE="ok"/>
+<attribute NAME="annot_modTime_Freeplane" VALUE="Fri Jun 14 15:45:10 CEST 2024"/>
+</node>
+<node TEXT="nested if" ID="ID_370209609" CREATED="1718372620796" MODIFIED="1718372620799" LINK="menuitem:_H2aOpenPdfOnAnnotPage_on_single_node">
+<attribute NAME="annot_page" VALUE="21" OBJECT="org.freeplane.features.format.FormattedNumber|21"/>
+<attribute NAME="annot_type" VALUE="note"/>
+<attribute NAME="annot_ID" VALUE="okular-{80c3a899-f8d4-41de-8b92-22392e31199a}"/>
+<attribute NAME="annot_modTime_PDF" VALUE="D:20240614154338+02&apos;00"/>
+<attribute NAME="annot_status" VALUE="ok"/>
+<attribute NAME="annot_modTime_Freeplane" VALUE="Fri Jun 14 15:43:40 CEST 2024"/>
+</node>
+<node TEXT="Rg ⊨ Jgd wgp (k Dδϕ.Dϕ - δϕ λ Q)" ID="ID_88802408" CREATED="1718372620804" MODIFIED="1718372710302" LINK="menuitem:_H2aOpenPdfOnAnnotPage_on_single_node">
+<attribute NAME="annot_page" VALUE="215" OBJECT="org.freeplane.features.format.FormattedNumber|215"/>
+<attribute NAME="annot_type" VALUE="highlight"/>
+<attribute NAME="annot_ID" VALUE="3a5abfdc-3380-432e-ae0f-2d87e116e1dc"/>
+<attribute NAME="annot_modTime_PDF" VALUE="D:20240614154510+01&apos;00"/>
+<attribute NAME="annot_status" VALUE="ok"/>
+<attribute NAME="annot_modTime_Freeplane" VALUE="Fri Jun 14 15:45:10 CEST 2024"/>
+</node>
+</node>
+</node>
+</node>
+</map>
